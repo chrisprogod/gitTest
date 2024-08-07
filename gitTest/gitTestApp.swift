e0d5f34b615b6f9909477cebe83a7d253f895b5a
Christian <cteguimtu620@gmail.com>
2024-08-07 13:53:52 -0400

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Christian Teguim on 2024-08-07.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
